53a88aee694a662b1d3094f5d604052da68d4d97
kraused53 <kraused53@gmail.com>
2022-07-09 19:06:21 -0400

    Laid out program steps in comments
    Also, sectioned off main program area in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+if __name__ == '__main__':
+    print('First Step: Empty Program')
+
+    # Use IP Geolocation to get Lat/Long estimate
+
+    # Feed this Lat/Long into weather API
+
+    # Parse JSON response
+
+    # Generate file name based on weather and time of day
+
+    # (OPTIONAL) Add text over image to display time and exact weather conditions
+
+    # Apply new desktop image
+
+    # Profit
