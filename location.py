aca5dfdfa4ea13c48e861898395a8bb80da8e2d4
kraused53 <kraused53@gmail.com>
2022-07-10 12:58:51 -0400

    Added comments to location.py functions

--- location.py
+++ location.py
@@ -1,5 +1,14 @@
 import geocoder
 
+'''
+	get_location():
+		Use teh geocoder API to estimate user's
+		current location using their computer's
+		IP address.
+
+		Returns a list containing location information
+			['City, State', [lat, long]]
+'''
 def get_location():
 	g = geocoder.ip('me')
 	data = []
@@ -7,6 +16,6 @@ def get_location():
 	data.append(g.latlng)
 	return data
 
-
+# If location.py is run as main, print location to terminal
 if __name__ == '__main__':
 	print(get_location())
