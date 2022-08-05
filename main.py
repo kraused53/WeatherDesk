0ddebba68917dc04f374adbd80348ac22b2da041
kraused53 <kraused53@gmail.com>
2022-08-05 17:45:26 -0400

    Program can now select the proper image
    If the specific weather code is does not have an image:
    It will select the default image for the weather group

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import geocoder
 # Used to get weather request
 import requests
 from SECRETS import OW_APIKEY
+# Used to check if files exist
+from os.path import exists
+from SECRETS import PATH_TO_IMAGES
+# Used to set the background image
+import subprocess
 
 '''
 
@@ -102,6 +107,70 @@ def get_forecast(latlng):
 
 
 '''
+
+    Step 3: Select an image
+
+        This function takes the user's weather forecast and selects the 
+        proper image
+
+    return:
+        Success -> [path to image, [errors]]
+        Failure -> None
+
+'''
+def get_image_path(data):
+    # set default time
+    t = 'day'
+
+    # If we have all time information
+    if (data[2] is not None)and(data[2] is not None)and(data[2] is not None):
+        # Determine time of day
+
+        # Dawn if between sr - 2 hours and sr
+        if (data[0] >= (data[1]- 7200)) and (data[0] < data[1]):
+            t = 'dawn'
+        # Morning if between sr and sr + 2 hours
+        elif (data[0] >= data[1]) and (data[0] < (data[1]+ 7200)):
+            t = 'morning'
+        # Day if between sr + 2 hours and ss - 2 hours
+        elif (data[0] >= (data[1]+ 7200)) and (data[0] < (data[2]- 7200)):
+            t = 'day'
+        # Evening
+        elif (data[0] >= (data[2]- 7200)) and (data[0] < data[2]):
+            t = 'evening'
+        # Dusk
+        elif (data[0] >= data[2]) and (data[0] < (data[2]+ 7200)):
+            t = 'dusk'
+        # Night
+        else:
+            t = 'night'
+
+    # Check for weather code
+    if data[3] is not None:
+        weather_group = str(int(data[3] / 100) * 100)
+    else:
+        weather_group = '800'
+
+    image_name = '/{wc}-{t}.jpg'.format(wc=str(data[3]), t=t)
+
+    # Check for weather group folder
+    path = PATH_TO_IMAGES + weather_group
+    if exists(path):
+        # Check for specific weather code
+        if exists(path + '/' + str(data[3])):
+            # Check for specific image
+            if exists(path + '/' + str(data[3]) + image_name):
+                return path + '/' + str(data[3]) + image_name
+            # If the specific image does not exist, use default for group
+            else:
+                image_name = '/{wc}-{t}.jpg'.format(wc=weather_group, t=t)
+                if exists(path + '/' + weather_group + image_name):
+                    return path + '/' + weather_group + image_name
+    
+    # Check to see is
+    return None
+
+'''
     Everything below runs whenever main.py is run
 '''
 if __name__ == '__main__':
@@ -117,3 +186,26 @@ if __name__ == '__main__':
     print('Step 2: Get forecast')
     print(forecast)
     print(' ')
+
+    # Step 3: Select an image
+    image_path = get_image_path(forecast)
+    print('Step 3: Select image')
+    print(image_path)
+    print(' ')
+
+    # Step 4: Create image overlay
+    print('Step 4: Create image overlay')
+    print('SKIP FOR NOW')
+    print(' ')
+
+    # Step 5: Apply new desktop
+    print('Step 5: Set background image')
+    print(' ')
+    subprocess.run(
+        [
+            '/usr/bin/gsettings', 
+            'set', 
+            'org.gnome.desktop.background', 
+            'picture-uri', 
+            image_path
+        ], capture_output=True)
